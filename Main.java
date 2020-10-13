dd54a924529faf067fe9cb60a99047921e947215
zesha5816 <zesha5816@wrdsb.ca>
2020-10-13 15:02:41 +0000

    Finished example

--- Main.java
+++ Main.java
@@ -1,15 +1,28 @@
 /**
- *
- * @author 
+ * produce a pattern of a random number of asterisks between 1 and 5 along the page
+ * @author Ahmad Zeshan
  */
 public class Main {
-
+  
+	public static void chaotic (int inputvalue){
+		for(int i = 1; i <= inputvalue; i++ ){
+			int randNum = (int)(Math.random()*(5 - 1 + 1)) + 1; //make the random number
+      //make a loop to print out the stars
+      for(int j = 0; j < randNum; j++){ 
+				//repeat a random number of times between 1 and 5
+        //print a star
+				System.out.print("*"); 
+      }
+			//move over one line
+      System.out.println(""); 
+    }
+  }
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+    // input the numbr of lines
+    chaotic(11);
+}
 }
